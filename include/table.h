7ecba74d584cd2b7976ffff20fdaacba8c7453cb
novivan <nov_ivan_777@mail.ru>
2024-11-26 20:04:51 +0300

    memory troubles may be fixed

--- include/table.h
+++ include/table.h
@@ -123,7 +123,7 @@ public:
                 for (auto& [columnName, column] : columns) 
                 {
                     column.cells[i].reset();
-                    column.cells.erase(column.cells.begin() + i);
+                    //column.cells.erase(column.cells.begin() + i);
                 }
                 --i;
                 --rowCount;
